ad147ff0887f25966c474a7f7a209f845eae3e94
shihujiang <jiangshihu@hotmail.com>
2019-02-15 15:13:05 +0800

    init project

---
+++ src/main/java/com/aiinspur/learndemo/learndemo/LearnDemoApplication.java
@@ -0,0 +1,14 @@
+package com.aiinspur.learndemo.learndemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnDemoApplication.class, args);
+	}
+
+}
+
